ec9fc11be9fa2bc9eaf0ea1e7a3bb5818410ac82
magickw <peterguo1983@gmail.com>
2022-02-27 21:23:43 -0800

    updated src-sw.js.

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,5 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => request.destination === 'image', pageCache);
