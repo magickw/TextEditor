5564bb58ac3265227e565689ead81d19e8ef7307
magickw <peterguo1983@gmail.com>
2022-02-28 15:14:58 -0800

    updated webpack.config.js.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -53,6 +53,20 @@ module.exports = () => {
         {
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
         }
         
       ],
